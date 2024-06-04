b11801118d29cb4bbfb9ee1dcc313041ebe9c8d1
penekhun <penekhun@gmail.com>
2024-06-04 18:39:27 +0900

    Polish "Maintain test for db47f950"

--- springdog-project/springdog-integration-tests/src/test/resources/Initial_data/predefined-rulesets.sql
+++ springdog-project/springdog-integration-tests/src/test/resources/Initial_data/predefined-rulesets.sql
@@ -1,6 +1,5 @@
 UPDATE APP.ENDPOINT
-SET ISPATTERNPATH = false,
-    BANTIMEINSECONDS = 0,
+SET BANTIMEINSECONDS = 0,
     IPBASED = false,
     PERMANENTBAN = false,
     REQUESTLIMITCOUNT = 0,
@@ -9,8 +8,7 @@ SET ISPATTERNPATH = false,
 WHERE FQCN = 'org.easypeelsecurity.springdogtest.ExampleController.example3';
 
 UPDATE APP.ENDPOINT
-SET ISPATTERNPATH = false,
-    BANTIMEINSECONDS = 3600,
+SET BANTIMEINSECONDS = 3600,
     IPBASED = true,
     PERMANENTBAN = false,
     REQUESTLIMITCOUNT = 100,
@@ -19,8 +17,7 @@ SET ISPATTERNPATH = false,
 WHERE FQCN = 'org.easypeelsecurity.springdogtest.ExampleController.example2';
 
 UPDATE APP.ENDPOINT
-SET ISPATTERNPATH = true,
-    BANTIMEINSECONDS = 0,
+SET BANTIMEINSECONDS = 0,
     IPBASED = true,
     PERMANENTBAN = false,
     REQUESTLIMITCOUNT = 0,
@@ -29,8 +26,7 @@ SET ISPATTERNPATH = true,
 WHERE FQCN = 'org.easypeelsecurity.springdogtest.ExampleController.example5';
 
 UPDATE APP.ENDPOINT
-SET ISPATTERNPATH = false,
-    BANTIMEINSECONDS = 3600,
+SET BANTIMEINSECONDS = 3600,
     IPBASED = true,
     PERMANENTBAN = false,
     REQUESTLIMITCOUNT = 100,
@@ -39,8 +35,7 @@ SET ISPATTERNPATH = false,
 WHERE FQCN = 'org.easypeelsecurity.springdogtest.ExampleController.example4';
 
 UPDATE APP.ENDPOINT
-SET ISPATTERNPATH = false,
-    BANTIMEINSECONDS = 3600,
+SET BANTIMEINSECONDS = 3600,
     IPBASED = true,
     PERMANENTBAN = false,
     REQUESTLIMITCOUNT = 100,
