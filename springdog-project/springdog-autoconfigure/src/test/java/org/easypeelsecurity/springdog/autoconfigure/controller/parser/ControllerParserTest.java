461e5391b31546020d96f6c6b9f2e1a1f91c5428
penekhun <penekhun@gmail.com>
2024-06-15 14:10:53 +0900

    Add Controller Parse unit tests

--- springdog-project/springdog-autoconfigure/src/test/java/org/easypeelsecurity/springdog/autoconfigure/controller/parser/ControllerParserTest.java
+++ springdog-project/springdog-autoconfigure/src/test/java/org/easypeelsecurity/springdog/autoconfigure/controller/parser/ControllerParserTest.java
@@ -17,30 +17,37 @@
 package org.easypeelsecurity.springdog.autoconfigure.controller.parser;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyList;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.only;
+import static org.assertj.core.api.AssertionsForClassTypes.tuple;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
 
 import java.lang.reflect.Method;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 import org.easypeelsecurity.springdog.manager.ratelimit.EndpointCommand;
 import org.easypeelsecurity.springdog.manager.ratelimit.EndpointQuery;
 import org.easypeelsecurity.springdog.manager.ratelimit.EndpointRepository;
 import org.easypeelsecurity.springdog.manager.ratelimit.VersionControlRepository;
 import org.easypeelsecurity.springdog.shared.configuration.SpringdogProperties;
+import org.easypeelsecurity.springdog.shared.ratelimit.EndpointDto;
 import org.easypeelsecurity.springdog.shared.ratelimit.VersionCompare;
 import org.easypeelsecurity.springdog.shared.ratelimit.model.EndpointVersionControl;
+import org.easypeelsecurity.springdog.shared.ratelimit.model.HttpMethod;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.mvc.condition.RequestMethodsRequestCondition;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 import jakarta.annotation.PostConstruct;
@@ -76,18 +83,25 @@ class ControllerParserTest {
             versionControlRepository);
   }
 
+  private final class MockHandlerMethod {
+
+    public void method() {
+    }
+  }
+
   @Test
   void testListEndpointsAndParameters_NoChanges() {
     // given
     when(handlerMapping.getHandlerMethods()).thenReturn(new HashMap<>());
     when(properties.computeAbsolutePath("/")).thenReturn("/");
-    when(endpointQuery.compareToLatestVersion(anyString(), any())).thenReturn(VersionCompare.SAME);
+    when(endpointQuery.compareToLatestVersion(ArgumentMatchers.anyString(), ArgumentMatchers.any())).thenReturn(
+        VersionCompare.SAME);
 
     // when
     controllerParser.listEndpointsAndParameters();
 
     // then
-    verify(versionControlRepository, never()).save(any(EndpointVersionControl.class));
+    verify(versionControlRepository, Mockito.never()).save(ArgumentMatchers.any(EndpointVersionControl.class));
     verifyNoInteractions(endpointRepository, endpointCommand);
   }
 
@@ -96,13 +110,14 @@ class ControllerParserTest {
     // given
     when(handlerMapping.getHandlerMethods()).thenReturn(new HashMap<>());
     when(properties.computeAbsolutePath("/")).thenReturn("/");
-    when(endpointQuery.compareToLatestVersion(anyString(), any())).thenReturn(VersionCompare.FIRST_RUN);
+    when(endpointQuery.compareToLatestVersion(ArgumentMatchers.anyString(), ArgumentMatchers.any())).thenReturn(
+        VersionCompare.FIRST_RUN);
 
     // when
     controllerParser.listEndpointsAndParameters();
 
     // then
-    verify(endpointRepository, only()).saveAll(anyList());
+    verify(endpointRepository, Mockito.only()).saveAll(ArgumentMatchers.anyList());
   }
 
   @Test
@@ -110,13 +125,57 @@ class ControllerParserTest {
     // given
     when(handlerMapping.getHandlerMethods()).thenReturn(new HashMap<>());
     when(properties.computeAbsolutePath("/")).thenReturn("/");
-    when(endpointQuery.compareToLatestVersion(anyString(), any())).thenReturn(VersionCompare.DIFFERENT);
+    when(endpointQuery.compareToLatestVersion(ArgumentMatchers.anyString(), ArgumentMatchers.any())).thenReturn(
+        VersionCompare.DIFFERENT);
 
     // when
     controllerParser.listEndpointsAndParameters();
 
     // then
-    verify(endpointCommand, only()).applyChanges(any(), any(), any());
+    verify(endpointCommand, Mockito.only()).applyChanges(ArgumentMatchers.any(), ArgumentMatchers.any(),
+        ArgumentMatchers.any());
+  }
+
+  @Test
+  @DisplayName("parseController return endpoints parsed.")
+  void controllerParse() throws NoSuchMethodException {
+    // given
+    RequestMappingInfo requestMappingInfo = Mockito.mock(RequestMappingInfo.class);
+    HandlerMethod handlerMethod = new HandlerMethod(new MockHandlerMethod(), "method");
+    when(requestMappingInfo.getDirectPaths()).thenReturn(Collections.singleton("/api/test"));
+    when(requestMappingInfo.getMethodsCondition()).thenReturn(
+        new RequestMethodsRequestCondition(RequestMethod.resolve(org.springframework.http.HttpMethod.GET)));
+    Map<RequestMappingInfo, HandlerMethod> handlerMethods = new HashMap<>();
+    handlerMethods.put(requestMappingInfo, handlerMethod);
+
+    // when
+    Set<EndpointDto> result = controllerParser.parseController(handlerMethods, "/exclude");
+
+    // then
+    assertThat(result.toArray())
+        .extracting("path", "httpMethod", "fqcn")
+        .containsExactly(
+            tuple("/api/test", HttpMethod.GET,
+                "org.easypeelsecurity.springdog.autoconfigure.controller.parser.MockHandlerMethod.method"));
+  }
+
+  @Test
+  @DisplayName("Endpoints with certain prefixes in the path should not be parsed.")
+  void controllerParseExclude() throws NoSuchMethodException {
+    // given
+    RequestMappingInfo requestMappingInfo = Mockito.mock(RequestMappingInfo.class);
+    HandlerMethod handlerMethod = new HandlerMethod(new MockHandlerMethod(), "method");
+    when(requestMappingInfo.getDirectPaths()).thenReturn(Collections.singleton("/exclude/api/test"));
+    when(requestMappingInfo.getMethodsCondition()).thenReturn(
+        new RequestMethodsRequestCondition(RequestMethod.resolve(org.springframework.http.HttpMethod.GET)));
+    Map<RequestMappingInfo, HandlerMethod> handlerMethods = new HashMap<>();
+    handlerMethods.put(requestMappingInfo, handlerMethod);
+
+    // when
+    Set<EndpointDto> result = controllerParser.parseController(handlerMethods, "/exclude");
+
+    // then
+    assertThat(result.size()).isZero();
   }
 
   @Test
